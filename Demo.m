c2c4de3aaadf4275acf24f054577ef091bf237a5
soorajsunil <soorajpro@gmail.com>
2024-05-03 00:37:07 -0400

    Add files via upload

---
+++ Demo.m
@@ -0,0 +1,9 @@
+clc; clear; close all;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data    = load('LFP.mat'); 
+[xm,ym] = dynamicProg(data.xn,data.yn,10); 
+
+figure; hold on; box on; grid on; xlabel('SOC (%)'); ylabel('OCV (V)'); 
+plot(data.xn*100,data.yn,'o')
+plot(xm*100,ym,'*--',LineWidth=2,MarkerSize=10)
+
