191888b23a58a2c5a4fafe4b0afc13174bfbfee7
Sooraj Sunil <soorajpro@gmail.com>
2025-04-07 21:29:02 -0400

    Version 1.0 - Major Updates

--- Demo.m
+++ Demo.m
@@ -1,10 +1,17 @@
 clc; clear; close all;
+set(groot,'defaultTextInterpreter','latex');
+set(groot,'defaultAxesTickLabelInterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-data    = load('LFP.mat'); 
+file_name = 'Molicel-INR21700P42A';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data    = load(strcat('Data/',file_name,'.mat')); 
 [xm,ym] = dynamicProg(data.xn,data.yn,10); 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure; hold on; box on; grid on; xlabel('SOC (%)'); ylabel('OCV (V)'); 
+f=figure; hold on; box on; grid on; xlabel('SOC (\%)'); ylabel('OCV (V)'); 
 plot(data.xn*100,data.yn,'o')
 plot(xm*100,ym,'*--',LineWidth=2,MarkerSize=10)
+title(file_name); exportgraphics(f,strcat('Figures/',file_name,'.png'))
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
