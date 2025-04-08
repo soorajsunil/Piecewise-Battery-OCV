73dafef00eff987b9781e98d078778721814487e
Sooraj Sunil <soorajpro@gmail.com>
2025-04-07 22:15:55 -0400

    Updated plot style.

--- Demo.m
+++ Demo.m
@@ -1,15 +1,12 @@
-clc; clear; close all;
-set(groot,'defaultTextInterpreter','latex');
-set(groot,'defaultAxesTickLabelInterpreter','latex');
-set(groot,'defaultLegendInterpreter','latex');
-
+run('Figures/init_plot.m');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-file_name = 'Samsung-INR2170040T';
+file_name = 'Molicel-INR21700P42A';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data    = load(strcat('Data/',file_name,'.mat')); 
 [xm,ym] = dynamicProg(data.xn,data.yn,10); 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-f=figure; hold on; box on; grid on; xlabel('SOC (\%)'); ylabel('OCV (V)'); 
+f=figure; hold on; box on; grid on; axis('padded')
+xlabel('SOC (\%)'); ylabel('OCV (V)'); 
 plot(data.xn*100,data.yn,'o')
 plot(xm*100,ym,'*--',LineWidth=2,MarkerSize=10)
 title(file_name); exportgraphics(f,strcat('Figures/',file_name,'.png'))
